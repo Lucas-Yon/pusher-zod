0a1e1c2667054fcffbd71daf40406684461e1458
Lucas <lucas@shopand.co>
2024-01-17 16:09:02 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,148 @@
+# Pusher-Zod
+
+Pusher-Zod is a TypeScript package designed to enhance type safety for both server and client implementations when using Pusher. By integrating the powerful Zod library for runtime type validation, Pusher-Zod ensures that your Pusher event data adheres to the expected types, reducing the risk of runtime errors and providing a more robust development experience.
+
+## Installation
+
+To install Pusher-Zod, use your package manager of choice:
+
+```bash
+npm install pusher-zod
+# or
+yarn add pusher-zod
+```
+
+## Usage
+
+### First create a setup object that defines your Pusher channels and events with Zod schemas:
+
+```typescript
+import pusherZod from "pusher-zod";
+import { z } from "zod";
+const setup = {
+  chat: {
+    message: z.object({
+      user: z.string(),
+      text: z.string(),
+    }),
+    messageDeleted: z.object({
+      user: z.string(),
+      messageId: z.string(),
+    }),
+  },
+  // Add more channels and events as needed
+};
+
+// Define if you need the users events ** optional **
+
+const userEvents = {
+  typing: z.object({
+    user: z.string(),
+  }),
+  notification: z.object({
+    user: z.string(),
+    text: z.string(),
+  }),
+  booleanEvents: z.boolean(),
+  numberEvents: z.number(),
+  // Add more events as needed
+};
+
+// Export the client and server and import them where you need them
+export const { TypeSafePusherClient, TypeSafePusherServer } = pusherZod({
+  setup,
+  userEvents, // optional
+  channel_id_separator: "_", // optional choose your own separator a channel will be call chat_conversationID for example
+});
+```
+
+### Then create a Pusher client instance using the TypeSafePusherClient class. See the PusherJS [docs](https://pusher.com/docs/channels) for more information
+
+```typescript
+import { TypeSafePusherClient } from "./pusher-zod-setup";
+
+// For the options check the pusher-js docs
+const Client = new TypeSafePusherClient({
+    "app_key", // your app key
+    // ...your options
+})
+
+// ubscribe to a channel. You will get an error if you try to join a channel that is not defined. The ID can be any value you want.
+const channel = Client.joinChannel("chat", "conversationID");
+
+// listen to an event
+channel.listen("message", (data) => {
+  // data is parsed and validated using safeParse from Zod
+  if(!data.success) return console.log("error");
+
+  // data is now typed and safe
+  console.log(data.user, data.text);
+});
+
+```
+
+### Then create a Pusher server instance using the TypeSafePusherServer class:
+
+```typescript
+import { TypeSafePusherServer } from "./pusher-zod-setup";
+
+// For the options check the pusher-js docs
+const pusherServer = new TypeSafePusherServer({
+  appId: "YOUR_PUSHER_APP_ID",
+  key: "YOUR_PUSHER_KEY",
+  secret: "YOUR_PUSHER_SECRET",
+  cluster: "YOUR_PUSHER_CLUSTER",
+});
+
+//  Use the invoke method for a type-safe way to trigger events
+
+await pusherServer.invoke("channelName", "channelID", "event", {
+  user: "user",
+  text: "text",
+});
+
+// example:
+
+await pusherServer.invoke("chat", "conversation1", "message", {
+  user: "user",
+  text: "text",
+});
+
+// Invoke like trigger also support multiple channels but instead you need to pass multiple channel ids
+
+await pusherServer.invoke("chat", ["conversation1","conversation2","conversation3"], "message", {
+  user: "user",
+  text: "te
+// sendToUser method is also now typesafe
+
+await pusherServer.sendToUser(
+  "userID",
+  "event",
+  "data matching the event schema"
+);
+
+// example:
+
+await pusherServer.sendToUser("1", "typing", {
+  user: "random username",
+});
+```
+
+## Ressources
+
+- [Pusher](https://pusher.com/)
+- [Zod](https://zod.dev)
+
+## Features
+
+Type-Safe Event Data: Ensure that event data sent and received through Pusher adheres to predefined TypeScript types.
+Server and Client Integration: Apply type safety both on the server and client sides for a seamless end-to-end development experience.
+Zod Integration: Leverage the Zod library for runtime type validation, making it easy to define and enforce data schemas.
+
+## Contributing
+
+Feel free to contribute to Pusher-Zod by opening issues or pull requests on the GitHub repository.
+
+## License
+
+Pusher-Zod is licensed under the [MIT License](LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,157 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import PusherClient, { type Channel as PusherChannel } from "pusher-js";
+import PusherServer from "pusher";
+
+import type { z, ZodType, SafeParseReturnType } from "zod";
+export default function <
+  S extends Record<string, Record<string, z.ZodType<any, any>>>,
+  P extends Partial<Record<string, z.ZodType<any, any>>> = Record<string, never>
+>({
+  setup,
+  channel_id_separator = ".",
+  userEvents = {} as P,
+}: {
+  setup: S;
+  channel_id_separator?: "_" | "-" | "=" | "@" | "," | "." | ";";
+  userEvents?: P;
+}) {
+  type ChannelsNames = keyof S;
+  type EventKeys<K extends ChannelsNames> = {
+    [P in keyof (typeof setup)[K]]: (typeof setup)[K][P] extends ZodType<
+      any,
+      any
+    >
+      ? P
+      : never;
+  }[keyof (typeof setup)[K]];
+
+  type MyChannel<K extends ChannelsNames> = {
+    bind<E extends EventKeys<K>>(
+      eventName: E,
+      callback: (data: S[K][E]) => void,
+      context?: any
+    ): MyPusherClient;
+    unbind<E extends EventKeys<K>>(
+      eventName?: E,
+      callback?: (data: S[K][E]) => void,
+      context?: any
+    ): MyPusherClient;
+    listen<E extends EventKeys<K>>(
+      eventName: E,
+      callback: (data: SafeParseReturnType<S[K][E], S[K][E]>) => void,
+      context?: any
+    ): void;
+  } & PusherChannel;
+
+  type MyPusherClient = Omit<PusherClient, "getClientFeatures"> & {
+    joinChannel<K extends ChannelsNames>(
+      channelName: K,
+      id: string | number
+    ): MyChannel<K>;
+    leaveChannel<K extends ChannelsNames>(
+      channelName: K,
+      id: string | number
+    ): void;
+    member: {
+      bind: <E extends Extract<keyof P, string>>(
+        eventName: E,
+        callback: (data: SafeParseReturnType<P[E], P[E]>) => void,
+        context?: any
+      ) => any;
+    };
+  };
+
+  class TypeSafePusherClient extends PusherClient implements MyPusherClient {
+    public member;
+
+    constructor(...args: ConstructorParameters<typeof PusherClient>) {
+      super(...args);
+      this.member = {
+        ...this.user,
+        bind: <E extends keyof P>(
+          eventName: E,
+          callback: (data: SafeParseReturnType<P[E], P[E]>) => void,
+          context?: any
+        ) => {
+          const parsedCallback = (data: P[typeof eventName]) => {
+            callback(parseData(userEvents[eventName]!, data));
+          };
+          return this.user.bind(String(eventName), parsedCallback, context);
+        },
+      };
+    }
+
+    /**
+     *
+     * @param channelName  existing channel name // add prefix private- or presence-
+     * @param id  identifier of the channel ( room id, user id, etc...)
+     * @returns  channel with classic pusher methods. Use listen to get type safe data
+     */
+    joinChannel<K extends ChannelsNames>(
+      channelName: K,
+      id: string | number
+    ): MyChannel<K> {
+      const channel = super.subscribe(
+        `${String(channelName)}${channel_id_separator}${id}`
+      ) as MyChannel<K>;
+      channel.listen = (eventName, callback, context) => {
+        const parsedCallback = (data: S[K][typeof eventName]) => {
+          callback(parseData(setup[channelName]![eventName]!, data));
+        };
+        channel.bind(eventName, parsedCallback, context);
+      };
+      return channel;
+    }
+    /** same logic as joinChannel  */
+    leaveChannel<K extends ChannelsNames>(
+      channelName: K,
+      id: string | number
+    ): void {
+      super.unsubscribe(`${String(channelName)}${channel_id_separator}${id}`);
+    }
+  }
+
+  type MyPusherServer = PusherServer & {
+    trigger<K extends ChannelsNames, E extends EventKeys<K>>(
+      channelName: K,
+      eventName: E,
+      data: S[K][E]
+    ): void;
+  };
+
+  class TypeSafePusherServer extends PusherServer implements MyPusherServer {
+    invoke<K extends ChannelsNames, E extends EventKeys<K>>(
+      channelName: K,
+      channel_id: string | number | Array<string | number>,
+      eventName: E,
+      data: z.infer<(typeof setup)[K][E]>
+    ) {
+      if (Array.isArray(channel_id)) {
+        const channels = channel_id.map((id) => {
+          return `${String(channelName)}${channel_id_separator}${id}`;
+        });
+        return super.trigger(channels, String(eventName), data);
+      }
+      return super.trigger(
+        `${String(channelName)}${channel_id_separator}${channel_id}`,
+        String(eventName),
+        data
+      );
+    }
+    override sendToUser<E extends keyof P>(
+      userId: string,
+      event: Extract<E, string>,
+      data: z.infer<Exclude<P[E], undefined>>
+    ): Promise<PusherServer.Response> {
+      return super.sendToUser(userId, event, data);
+    }
+  }
+  function parseData<K>(zodSchema: z.ZodType<any, any>, data: K) {
+    return zodSchema.safeParse(data);
+  }
+
+  return {
+    TypeSafePusherClient,
+    TypeSafePusherServer,
+  };
+}
